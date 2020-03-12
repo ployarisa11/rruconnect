86f96f63b9ef08d986665e456597fcb6c4cb8e38
ployarisa11 <Ploy_arisa112@hotmail.com>
2020-03-12 11:07:11 +0700

    ยยย

--- index.js
+++ index.js
@@ -1,56 +1,82 @@
-/*
-  Webhook of Dialogflow
-  @author: NottDev
-  date: 31/05/2019
-*/
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const port = process.env.PORT || 4000;
 
-// Import the appropriate class
-const {
-  WebhookClient
-} = require('dialogflow-fulfillment');
+const firebase = require("firebase/app")
+require("firebase/firestore")
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+var cron = require('node-cron');
+const express = require('express')
+const app = express()
+
 
 app.get('/', (req, res) => {
-  res.send({
-    success: true
-  });
-})
+    res.json({ message: 'Ahoy!' })
+  })
 
-app.post('/webhook', (req, res) => {
-  console.log('POST: /');
-  console.log('Body: ',req.body);
+//firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyA5KFIcemUtm1_i64TUyifV0WfKjbm9irk",
+    authDomain: "rru-connect-epeevr.firebaseapp.com",
+    databaseURL: "https://rru-connect-epeevr.firebaseio.com",
+    projectId: "rru-connect-epeevr",
+    storageBucket: "rru-connect-epeevr.appspot.com",
+    messagingSenderId: "898597223567",
+    appId: "1:898597223567:web:8cd2b5064e8edbaf20c4fa"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  //Create an instance
-  const agent = new WebhookClient({
-    request: req,
-    response: res
-  });
+const db = firebase.firestore()
 
-  //Test get value of WebhookClient
-  console.log('agentVersion: ' + agent.agentVersion);
-  console.log('intent: ' + agent.intent);
-  console.log('locale: ' + agent.locale);
-  console.log('query: ', agent.query);
-  console.log('session: ', agent.session);
+const LineNotify = require("./src/client");
 
-  //Function Location
-  function location(agent) {
-    agent.add('Welcome to Thailand.');
-  }
+const ACCESS_TOKEN = "GU1WHyUZz9MopIWb4Z7g1soJT7dkuVUMAO3Drjhvy4y";
+const notify = new LineNotify(`${ACCESS_TOKEN}`);
 
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Location', location);  // "Location" is once Intent Name of Dialogflow Agent
-  agent.handleRequest(intentMap);
+
+
+//เอาชื่อมา
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+    channelAccessToken: 'UC877QAnBp4CE5A/3/LNgpLduTrTxEdqZRuF8ShT6p+PFg5A471cvw8buhkpTcRuRbRw3egWF+Y68p2k13FdY9OuYzONaFuYo2zWlDUiJMMHIKjtbqyENtxM1/X2xyjde+OkQyVnJy/6xKNXohqwdQdB04t89/1O/w1cDnyilFU='
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
+
+
+
+
+
+let Noti = db.collection('Count');
+
+cron.schedule('* * * * *', function () {
+    Noti.get().then((snapshot) => {
+        let pp = snapshot.docs[snapshot.docs.length - 1];
+        let noti = pp.data().count;
+
+        if (noti == 1) {
+            client.getProfile('U6d5ea64790214c58808993455916422f')
+                .then((profile) => {
+
+                    notify.sendText("กรุณาติดต่อกลับผู้ใช้งาน " + profile.displayName  + "  ด้วยค่ะ");
+
+                })
+                .catch((err) => {
+                    // error handling
+                });
+
+            Noti.doc('H0pK45spljceEu7DBwmP').update({
+                count: 0
+            });
+
+
+        } else {
+            console.log(pp.data().count);
+        }
+
+    });
+    console.log('running a task every two minutes');
 });
+
+
+app.listen(9000, () => {
+    console.log('Application is running on port 9000')
+  })
